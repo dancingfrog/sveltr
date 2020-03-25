99438798a40fcb059a8742cff1401b6f6b8dd26c
John <john@real-currents.com>
2020-03-25 16:10:52 -0400

    add builtin reference to basic shaders

--- src/apps/shaders/custom/basic-fragment-shader.glsl
+++ src/apps/shaders/custom/basic-fragment-shader.glsl
@@ -1,3 +1,29 @@
+/* This builtin is prepended to every
+ * custom vertext shader in Svelte:
+ */
+/* start builtins */
+//#extension GL_OES_standard_derivatives : enable
+
+//precision highp float;
+//
+//struct DirectionalLight {
+//	vec3 direction;
+//	vec3 color;
+//	float intensity;
+//};
+//
+//struct PointLight {
+//	vec3 location;
+//	vec3 color;
+//	float intensity;
+//// TODO fall-off etc
+//};
+//
+//uniform vec3 AMBIENT_LIGHT;
+//uniform DirectionalLight DIRECTIONAL_LIGHTS[NUM_LIGHTS];
+//uniform PointLight POINT_LIGHTS[NUM_LIGHTS];
+/* end builtins */
+
 uniform vec3 color;
 
 varying vec3 v_normal;
--- src/apps/shaders/custom/basic-vertex-shader.glsl
+++ src/apps/shaders/custom/basic-vertex-shader.glsl
@@ -1,3 +1,26 @@
+/* This builtin is prepended to every
+ * custom vertext shader in Svelte:
+ */
+/* start builtins */
+//precision highp float;
+//
+//uniform mat4 MODEL;
+//uniform mat4 PROJECTION;
+//uniform mat4 VIEW;
+//uniform mat4 MODEL_INVERSE_TRANSPOSE;
+//
+//uniform vec3 CAMERA_WORLD_POSITION;
+//
+//struct PointLight {
+//	vec3 location;
+//	vec3 color;
+//	float intensity;
+//// TODO fall-off etc
+//};
+//
+//uniform PointLight POINT_LIGHTS[NUM_LIGHTS];
+/* end builtins */
+
 attribute vec3 position;
 attribute vec3 normal;
 
