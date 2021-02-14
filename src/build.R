a0d8796170bb3a35c9b83ac9ea7133ee80444929
John <john@real-currents.com>
2021-02-13 21:01:25 -0500

    improve viz r example app

--- src/build.R
+++ src/build.R
@@ -1,5 +1,5 @@
 if (!require("blogdown")) {
-    devtools::install_version("blogdown", version = "0.20")
+    devtools::install_version("blogdown", version = "0.20", upgrade = FALSE)
     blogdown::install_hugo("0.48", force = TRUE, use_brew = FALSE)
 }
 
