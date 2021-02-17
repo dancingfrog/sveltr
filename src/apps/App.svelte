3d58e0c9a27abfe918729cd716da449432e3bb02
John <john@real-currents.com>
2021-02-17 06:56:24 -0500

    drawing scatter plot

--- src/apps/App.svelte
+++ src/apps/App.svelte
@@ -10,16 +10,93 @@
     let state = "";
     let pop = "";
 
-    let chartData = [
-      { name: 'data1'},
-      { name: 'data2'},
-      { name: 'data3'},
-      { name: 'data4'},
-      { name: 'data5'}
+    let chartData =  [
+      {
+        name: 'data1',
+        points: [
+          { x: 10, y: 8.04 },
+          { x: 8, y: 6.95 },
+          { x: 13, y: 7.58 },
+          { x: 9, y: 8.81 },
+          { x: 11, y: 8.33 },
+          { x: 14, y: 9.96 },
+          { x: 6, y: 7.24 },
+          { x: 4, y: 4.26 },
+          { x: 12, y: 10.84 },
+          { x: 7, y: 4.82 },
+          { x: 5, y: 5.68 }
+        ]
+      },{
+        name: 'data2',
+        points: [
+          { x: 10, y: 8.04 },
+          { x: 8, y: 6.95 },
+          { x: 13, y: 7.58 },
+          { x: 9, y: 8.81 },
+          { x: 11, y: 8.33 },
+          { x: 14, y: 9.96 },
+          { x: 6, y: 7.24 },
+          { x: 4, y: 4.26 },
+          { x: 12, y: 10.84 },
+          { x: 7, y: 4.82 },
+          { x: 5, y: 5.68 }
+        ]
+      },{
+        name: 'data3',
+        points: [
+          { x: 10, y: 8.04 },
+          { x: 8, y: 6.95 },
+          { x: 13, y: 7.58 },
+          { x: 9, y: 8.81 },
+          { x: 11, y: 8.33 },
+          { x: 14, y: 9.96 },
+          { x: 6, y: 7.24 },
+          { x: 4, y: 4.26 },
+          { x: 12, y: 10.84 },
+          { x: 7, y: 4.82 },
+          { x: 5, y: 5.68 }
+        ]
+      },{
+        name: 'data4',
+        points: [
+          { x: 10, y: 8.04 },
+          { x: 8, y: 6.95 },
+          { x: 13, y: 7.58 },
+          { x: 9, y: 8.81 },
+          { x: 11, y: 8.33 },
+          { x: 14, y: 9.96 },
+          { x: 6, y: 7.24 },
+          { x: 4, y: 4.26 },
+          { x: 12, y: 10.84 },
+          { x: 7, y: 4.82 },
+          { x: 5, y: 5.68 }
+        ]
+      },{
+        name: 'data5',
+        points: [
+          { x: 10, y: 8.04 },
+          { x: 8, y: 6.95 },
+          { x: 13, y: 7.58 },
+          { x: 9, y: 8.81 },
+          { x: 11, y: 8.33 },
+          { x: 14, y: 9.96 },
+          { x: 6, y: 7.24 },
+          { x: 4, y: 4.26 },
+          { x: 12, y: 10.84 },
+          { x: 7, y: 4.82 },
+          { x: 5, y: 5.68 }
+        ]
+      },
     ];
 
     let handleFeatureSearch = (evt) => {
-      console.log(`submitted ${chartData}`);
+      console.log(evt);
+      const data = evt['explicitOriginalTarget'].getAttribute('data');
+      try {
+        console.log(JSON.parse(data));
+      } catch (e) {
+        console.error(data);
+      }
     }
 
     let dod_color = '#c80e38';
